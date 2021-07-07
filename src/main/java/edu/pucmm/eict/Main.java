b99ff865243b85b444ce5fca0b20c15853b5c85b
Thevic16 <20171408@ce.pucmm.edu.do>
2021-07-07 14:36:19 -0400

    first commit

---
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -0,0 +1,21 @@
+package edu.pucmm.eict;
+
+import edu.pucmm.eict.util.EmailUtility;
+
+
+public class Main {
+    public static void main(String[] args) {
+        EmailUtility emailUtility = new EmailUtility();
+        emailUtility.sendMail( "victorjhoan2@gmail.com", "Prueba Java", "Proyecto ITT silla de ruedas inteligente" );
+
+    }
+
+}
+
+
+
+
+
+
+
+
